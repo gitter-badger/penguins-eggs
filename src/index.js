d5b300365dc80b79c592dcdc1ac1f71a188dbe6c
artisan <artisan@jasmine.lan>
2017-08-16 18:36:30 +0200

    rimosso: tempinstaller, corretto fstab, messa schermata di aiuto: FUNZIONA!!!

--- src/index.js
+++ src/index.js
@@ -5,7 +5,37 @@
 import { install } from "source-map-support";
 install();
 
+let version ="0.2.0";
+console.log(`#### incubator ${version} ####`);
+
+
 import Eggs from "./lib/Eggs.js";
+
+// Check for parameters
+let rebuild = false;
+let help=false;
+process.argv.forEach(function(val, index, array) {
+  if (val == "rebuild") {
+    rebuild = true;
+  } else if(val=="r"){
+    rebuild = true;
+  } else if(val=="help"){
+    help=true;
+  } else if(val=="h"){
+    help=true;
+  }
+});
+
+if(help){
+  console.log(`incubator version: ${version}`);
+  console.log(`Description: an utility to remaster your system and boot it from remote`);
+  console.log(`Usage: incubator [options]`);
+  console.log(`h,   help        this help`);
+  console.log(`r,   rebuild     destroy and rebuild all`);
+  console.log(`(C) 2017 piero.proietti@gmail.com`)
+  process.exit();
+}
+
 let shell = require("shelljs");
 
 var os = require("os");
@@ -15,39 +45,6 @@ console.log("platform: " + os.platform());
 console.log("arch: " + os.arch());
 console.log("release: " + os.release());
 
-var ifaces = os.networkInterfaces();
-Object.keys(ifaces).forEach(function(ifname) {
-  var alias = 0;
-  ifaces[ifname].forEach(function(iface) {
-    if ("IPv4" !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ":" + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-    }
-    ++alias;
-  });
-});
-
-// INSTALLAZIONE
-// conf /etc/fabricator
-// iso/* usr/lib/fabricator/iso
-
-let rebuild = false;
-process.argv.forEach(function(val, index, array) {
-  if (val == "rebuild") {
-    rebuild = true;
-    console.log("### rebuild ###");
-  }
-});
-
-console.log("Inizio");
 
 const distroName = "littlebird";
 const homeDir = "/srv/incubator/" + distroName;
@@ -66,25 +63,27 @@ e.workDirs();
 // System
 e.systemCopy();
 
-// Eggs
+// Eggs client
 e.fstab();
 e.hostname();
 e.resolvConf();
 e.interfaces();
 e.hosts();
-e.tempInstaller();
-e.tempInstallerMount();
-e.tempInstallerUmount();
-
-e.pxelinux();
-e.exports();
-e.dnsmasq();
 e.vmlinuz();
 e.initramfs();
 
-shell.exec("service dnsmasq restart");
+// Eggs server
+e.pxelinux();
+e.dnsmasq();
+e.exports();
+
+shell.exec("service dnsmasq stop");
+shell.exec("service dnsmasq start");
 // System
 
+//e.tempInstaller();
+//e.tempInstallerMount();
+//e.tempInstallerUmount();
 // f.systemClean();
 // f.systemEdit();
 //f.systemIsoName();
