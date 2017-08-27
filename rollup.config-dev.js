bf8476727eb2b9382c76d1d6344dc11c2c0bc401
artisan <artisan@jasmine.lan>
2017-08-27 16:41:19 +0200

    tentatives...

--- rollup.config-dev.js
+++ rollup.config-dev.js
@@ -15,40 +15,22 @@ export default {
     sourcemap: true
   },
   plugins: [
-    babel({
-      exclude: "node_modules/**"
-    }),
-    json(),
-    commonjs({
-      // non-CommonJS modules will be ignored, but you can also
-      // specifically include/exclude files
-      include: "node_modules/**", // Default: undefined
-      exclude: ["node_modules/foo/**", "node_modules/bar/**"], // Default: undefined
-      // these values can also be regular expressions
-      // include: /node_modules/
-
-      // search for files other than .js files (must already
-      // be transpiled by a previous plugin!)
-      extensions: [".js", ".coffee"], // Default: [ '.js' ]
-      // if true then uses of `global` won't be dealt with by this plugin
-      ignoreGlobal: false, // Default: false
-      // if false then skip sourceMap generation for CommonJS modules
-      sourceMap: false, // Default: true
-      // explicitly specify unresolvable named exports
-      // (see below for more details)
-      namedExports: { "./module.js": ["foo", "bar"] }, // Default: undefined
-      // sometimes you have to leave require statements
-      // unconverted. Pass an array containing the IDs
-      // or a `id => boolean` function. Only use this
-      // option if you know what you're doing!
-      ignore: ["conditional-runtime-dependency"]
-    }),
     nodeResolve({
       jsnext: true,
       main: true,
       browser: false,
-      extensions: [".js", ".json"] // Default: ['.js']
+      extensions: [".js", ".json"]
     }) ,
+    commonjs({
+      include: "node_modules/**",
+      extensions: [".js", ".coffee"],
+      ignoreGlobal: false,
+      sourceMap: true
+    }),
+    babel({
+      exclude: "node_modules/**"
+    }),
+    json(),
     nodeBuiltins(),
     nodeGlobals()
   ]
