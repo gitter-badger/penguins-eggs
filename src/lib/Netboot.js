2e4a5de1de885995a18f6ef1f85a0d4593cd8cb9
artisan <artisan@jasmine.lan>
2017-08-31 21:32:27 +0200

    revisione pagine pxelinux.cfg e isolinux.cfg

--- src/lib/Netboot.js
+++ src/lib/Netboot.js
@@ -108,7 +108,7 @@ PROMPT 0
 KBDMAP it.kbd
 DISPLAY display.txt
 SAY Uso la tastiera e locale per italiano.
-MENU TITLE Penguins eggs Booting from LAN 
+MENU TITLE Penguins eggs NETBOOT
 MENU BACKGROUND eggs.png
 
 LABEL ${this.distroName}
