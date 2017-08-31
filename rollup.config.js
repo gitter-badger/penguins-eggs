3337cbae1e30a179ff031188849d52075f545b8c
artisan <artisan@jasmine.lan>
2017-08-31 05:54:20 +0200

    well, bin is working for first time!

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,9 @@ import shebang from 'rollup-plugin-shebang';
 import json from "rollup-plugin-json";
 
 export default {
-  input: "src/index.js",
+  input: "./src/index.js",
   output: {
-    file: "build/teggs.js",
+    file: "./build/index.js",
     format: "cjs",
     name: "teggs",
     sourcemap: true
